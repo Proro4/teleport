0c44e641332c4bb16b1ce23274b2fae3f37adea2
Levitskiy Denis <Proro4@gmail.com>
2018-07-26 18:14:58 +0300

    add cloud page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+   let windowHeight = $(window).height();
+   let heightHeader = $('.header').height();
+   $('.wrapper').css('height',windowHeight - (heightHeader+5));
+   $(window).resize(function () {
+       let windowHeight = $(window).height();
+       let heightHeader = $('.header').height();
+       $('.wrapper').css('height',windowHeight - (heightHeader+5));
+   })
+});
