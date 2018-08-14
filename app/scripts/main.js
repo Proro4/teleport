4a7e7eb28182e40aba3f40d45269228164658f63
Levitsky Denis <levitsky.d@newway.com.ua>
2018-08-14 18:05:00 +0300

    last fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,5 +6,8 @@ $(document).ready(function () {
        let windowHeight = $(window).height();
        let heightHeader = $('.header').height();
        $('.wrapper').css('height',windowHeight - (heightHeader+5));
+   });
+   $('.backup__file input').change(function () {
+       $(this).parent().attr('title',($(this)[0].files[0].name));
    })
 });
